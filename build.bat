da0cba16aa0c3ec9629b9bc540bb6b55bdefc6fb
ChenHaoYan <chanhaoyan@126.com>
2022-11-06 11:43:30 +0800

    commit

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
 @echo off
 pyinstaller .\src\Main.py -F -w -p .\src\MainWindow.py -p .\src\SettingMenu.py -p .\src\Values.py -p .\src\Tools.py -p .\src\ImageData.py --upx-dir .\tools\upx --distpath .\dist
-del .\Main.spec
 pyinstaller .\src\Updater.py -F -w -p .\src\Values.py --upx-dir .\tools\upx --distpath .\dist
-del .\Updater.spec
